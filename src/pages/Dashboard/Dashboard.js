aedeb71a0481df8cf108d94cc59a4a3d497471f6
Gabriel Carlos Carvalho <gabrielcarlos@MacBook-Pro-de-Gabriel.local>
2022-08-15 13:56:20 -0400

    edit

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -38,7 +38,7 @@ const Dashboard = () => {
           <p>{post.title}</p>
           <div className="actions" >
             <Link to={`/posts/${post.id}`} className="btn btn-outline">Ver</Link>
-            <Link to={`/posts/${post.id}`} className="btn btn-outline">Editar</Link>
+            <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">Editar</Link>
             <button onClick={() => deleteDocument(post.id)} className="btn btn-outline btn-danger">Excluir</button>
           </div>
         </div>
